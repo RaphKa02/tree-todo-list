cad19ff6f7e1011a47e2438a32687a98a7e367b6
Raphael Karl <raphaelcom1@gmail.com>
2026-04-14 19:47:48 +0200

    Example

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+import { CatScratchEditorProvider } from './catScratchEditor.js';
+
+export function activate(context: vscode.ExtensionContext) {
+	// Register our custom editor providers
+	context.subscriptions.push(CatScratchEditorProvider.register(context));
+}
